4213075f07e9c0931bc9a493c533c6a253dfd38a
yum0e <yum0e@protonmail.com>
2022-04-24 22:13:54 +0200

    :construction: implemented increase_amount for testing

--- pool/src/governance_proxy.rs
+++ pool/src/governance_proxy.rs
@@ -2,12 +2,13 @@ elrond_wasm::imports!();
 
 #[elrond_wasm::proxy]
 pub trait GovernanceStaking {
+
     #[payable("*")]
-    #[endpoint(myEndpoint)]
-    fn my_payable_endpoint(
+    #[endpoint(increaseAmount)]
+    fn increase_amount(
         &self, 
         #[payment_token] payment_token: TokenIdentifier,
-        #[payment_nonce] payment_nonce: u64,
         #[payment_amount] payment_amount: BigUint,
-        arg: BigUint) -> BigUint;
+        timestamp: u64
+    ) -> SCResult<()>;
 }
--- pool/src/pool.rs
+++ pool/src/pool.rs
@@ -3,14 +3,14 @@
 elrond_wasm::imports!();
 
 mod governance_proxy;
-use governance_proxy::*;
 
-/// An empty contract. To be used as a template when starting a new contract from scratch.
+// governance testnet address : erd1qqqqqqqqqqqqqpgqvrc0x026cf44kktc7jhw6mgtpu9d5cw2j9tsfxnjta
+
 #[elrond_wasm::contract]
 pub trait Pool {
 
     #[proxy]
-    fn governance_sc(&self, sc_address: ManagedAddress) -> governance_proxy::Proxy<Self::Api>;
+    fn governance_sc_proxy(&self, sc_address: ManagedAddress) -> governance_proxy::Proxy<Self::Api>;
 
     #[init]
     fn init(
@@ -20,10 +20,27 @@ pub trait Pool {
         self.governance_sc_address().set(&governance_sc_address);
     }
 
+    #[payable("*")]
+    #[endpoint(increaseAmount)]
+    fn increase_amount(
+        &self,
+        #[payment_token] payment_token: TokenIdentifier,
+        #[payment_amount] payment_amount: BigUint,
+        timestamp: u64
+    ) -> SCResult<()> {
+        let governance_sc_address = self.governance_sc_address().get();
+        self.governance_sc_proxy(governance_sc_address)
+            .increase_amount(
+                payment_token,
+                payment_amount,
+                timestamp
+            )
+            .transfer_execute();
+        Ok(())
+    }
+
     // storage
 
     #[storage_mapper("governance_sc_address")]
     fn governance_sc_address(&self) -> SingleValueMapper<ManagedAddress>;
-
-
 }
